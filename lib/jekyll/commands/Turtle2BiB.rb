6a0236611dc50f2749d864e47487856aba3b71b4
RohanAsmat <m.rohan.a.asmat@gmail.com>
2017-11-07 01:18:55 +0100

    basic conditions for lines

--- lib/jekyll/commands/Turtle2BiB.rb
+++ lib/jekyll/commands/Turtle2BiB.rb
@@ -26,21 +26,47 @@ module Jekyll
               #in the newly created BibFile
                   File.open(turtleFilePath, "r") do |f|
                     f.each_line do |line|
+                      lineParser = line.split
+                      publicationCheck = lineParser[0]
+
+                      #Reading the further Lines of the Publication Entry
+                      if readingPublication == true
+                        #Write the entities of the Publication Entry to Bib
+                        if lineParser[0] == "dc:title"
+
+                        elsif lineParser[0] == "dc:date"
+
+                        elsif lineParser[0] == "bibo:pages"
+                          
+                        elsif lineParser[0] == "dc:publisher"
+
+                        elsif lineParser[0] == "foaf:Organization"
+
+                        elsif lineParser[0] == "dc:author"
+
+                        else
+
+                        end
+
+                        if( lineParser.last == "." )
+                          #last line of the Publication Entry
+                          readingPublication = false
+                          puts line
+                        end
+                      end
+
                        if line =~ /sdapublication:/
                          #puts "Found root: #{line}"
-                         lineParser = line.split
-                         publicationCheck = lineParser[0]
-
+                         #Reading the first line of the Publication Enrty
                          if ( lineParser[1] == "a" ) && ( lineParser[3] == ";" ) && ( publicationCheck.include? resourceIdentifier )
                            if ( lineParser[2] == "bibo:Article")
-                             readingPublication == true
-                             countPublication = countPublication + 1 
+                             readingPublication = true
+                             countPublication = countPublication + 1
                              puts lineParser
                            end
                          end
-
-
                        end
+
                     end
                     if countPublication == 0
                       puts "No publication found in the Turtle File."
